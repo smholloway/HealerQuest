3d6f8690d54613c6a1f35a8a622f59648fbda3f1
Seth Holloway <smholloway@gmail.com>
2011-03-20 11:31:54 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealerQuest::Application.config.secret_token = '955e7ff06d257f4625373bb0a33df4354d2672d361265406957b1a98ccc9b45d78ed52e0329b938a9e94623a99a50cd77da3e52c8d367a6eee62c65a3f9ed19c'
